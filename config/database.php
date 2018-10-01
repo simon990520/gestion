03720651b56fca19e9af50316b1d60a4779bf99b
simon990520 <simontrinidad.f.b@gmail.com>
2018-10-01 14:44:57 -0500

    My first commit

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
